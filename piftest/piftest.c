b771f842f928fd6f912665377bcab68f22aa1926
deglingo <ahmidmefes@gmail.com>
2014-09-19 19:42:45 +0200

    disabled the --track-fds option for valgrind

--- piftest/piftest.c
+++ piftest/piftest.c
@@ -95,7 +95,9 @@ static char **_valgrind_command ( PifConfig *config )
   cmd[nargs++] = "--quiet";
   cmd[nargs++] = "--leak-check=yes";
   cmd[nargs++] = "--fullpath-after=";
-  cmd[nargs++] = "--track-fds=yes";
+  /* [FIXME] this option looks usefull but the produced traceback is
+     not clear */
+  /* cmd[nargs++] = "--track-fds=yes"; */
   cmd[nargs++] = "--num-callers=50";
   cmd[nargs++] = "--show-below-main=yes";
   cmd[nargs++] = "--read-var-info=yes";
